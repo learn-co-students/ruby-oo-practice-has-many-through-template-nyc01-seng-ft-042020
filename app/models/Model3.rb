d9bc23ac1fab61a3c4c43351ea88b187c8b83176
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-26 15:47:30 -0400

    Done.

--- app/models/Model3.rb
+++ app/models/Model3.rb
@@ -1,5 +1,17 @@
 #class for Model3 goes here
 #Feel free to change the name of the class
-class Model3
+class Quantity
+    attr_reader :num, :unit
+    @@all = []
+    def initialize(num,unit)
+        @num = num
+        @unit = unit
+
+        Quantity.all<<self
+    end
+
+    def self.all
+        @@all
+    end
   
 end
