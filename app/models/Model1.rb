63e36366c8ce97a77bedcf437dce9d83d1ba48da
FCGit19 <canofred@gmail.com>
2020-04-28 18:20:26 -0400

    Done.

--- app/models/Model1.rb
+++ app/models/Model1.rb
@@ -1,5 +1,23 @@
 #class for Model1 goes here
 #Feel free to change the name of the class
-class Model1
+
+class Driver 
+  
+  def initialize(name)
+    @name = name 
+  end 
+
+  def rides #get the rides that the driver has driven
+    Ride.all.select do |ride|
+      ride.driver == self # this is calling the driver method on each ride instance, and comparing it to the driver we are calling rides on.
+    end 
+  end 
+
+  #make a method to find the passengers for the ride that belong to the driver
+  def passenger 
+    rides.collect do |ride|
+      ride.passenger
+    end 
+  end 
 
 end
--- app/models/Model2.rb
+++ app/models/Model2.rb
@@ -1,5 +1,20 @@
 #class for Model2 goes here
 #Feel free to change the name of the class
-class Model2
+class Ride
+  attr_reader :driver, :passenger #this is to get the driver of that ride instance. We call it on the ride instance 
+  @@all = []
+  def initialize(ride_id, driver, passenger)
+    @ride_id = ride_id
+    @driver = driver 
+    @passenger = passenger
+    Ride.all << self 
+  end 
+
+  def self.all #we need to return an array of all instances of the Ride class 
+    @@all 
+  end 
+
+  #write a  method that pulls the driver's object ID
+
 
 end
